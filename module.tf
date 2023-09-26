30d66fc7a432da4ad9327c77154abb88fa056617
m.Chaitanya <67100925+chaitanya2020j@users.noreply.github.com>
2023-09-26 22:44:54 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -2,3 +2,7 @@ module "naming" {
   source  = "Azure/naming/azurerm"
   version = "0.3.0"
 }
+resource "azurerm_resource_group" "example" {
+  name     = module.naming.resource_group.name
+  location = "West Europe"
+}
